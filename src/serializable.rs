3e45845eaf6deea2048ddc530d2a0c3c72787c00
wradley <wcallahan@wi.rr.com>
2020-08-15 17:50:07 -0500

    to bytes

--- src/serializable.rs
+++ src/serializable.rs
@@ -1,9 +1,18 @@
 
-pub unsafe fn to_bytes<T>(data: &T) -> &[u8] {
+pub unsafe fn to_byte_slice<T>(data: &T) -> &[u8] {
     std::slice::from_raw_parts(
         data as *const T as *const u8,
         std::mem::size_of::<T>()
     )
+}
+
+pub fn to_bytes<T>(data: &T) -> Vec<u8> {
+    let slice = unsafe { to_byte_slice(data) };
+    let mut bytes:Vec<u8> = Vec::new();
+    for byte in slice {
+        bytes.push(*byte);
+    }
+    bytes
 }
 
 trait Serializable {
